134319e89aa863176394b03f1b1ca41c9da4a1a5
Sudharsan2k20 <sudharsanm204@gmail.com>
2024-06-30 20:34:11 +0530

    Personal Details for Portfolio

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -5,96 +5,114 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <head>
 	<meta charset="utf-8">
 	<title>Welcome to CodeIgniter</title>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
+	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
 	<style type="text/css">
-
-	::selection { background-color: #E13300; color: white; }
-	::-moz-selection { background-color: #E13300; color: white; }
-
-	body {
-		background-color: #fff;
-		margin: 40px;
-		font: 13px/20px normal Helvetica, Arial, sans-serif;
-		color: #4F5155;
-	}
-
-	a {
-		color: #003399;
-		background-color: transparent;
-		font-weight: normal;
-		text-decoration: none;
-	}
-
-	a:hover {
-		color: #97310e;
-	}
-
-	h1 {
-		color: #444;
-		background-color: transparent;
-		border-bottom: 1px solid #D0D0D0;
-		font-size: 19px;
-		font-weight: normal;
-		margin: 0 0 14px 0;
-		padding: 14px 15px 10px 15px;
-	}
-
-	code {
-		font-family: Consolas, Monaco, Courier New, Courier, monospace;
-		font-size: 12px;
-		background-color: #f9f9f9;
-		border: 1px solid #D0D0D0;
-		color: #002166;
-		display: block;
-		margin: 14px 0 14px 0;
-		padding: 12px 10px 12px 10px;
-	}
-
-	#body {
-		margin: 0 15px 0 15px;
-		min-height: 96px;
-	}
-
-	p {
-		margin: 0 0 10px;
-		padding:0;
-	}
-
-	p.footer {
-		text-align: right;
-		font-size: 11px;
-		border-top: 1px solid #D0D0D0;
-		line-height: 32px;
-		padding: 0 10px 0 10px;
-		margin: 20px 0 0 0;
-	}
-
-	#container {
-		margin: 10px;
-		border: 1px solid #D0D0D0;
-		box-shadow: 0 0 8px #D0D0D0;
-	}
+	
 	</style>
 </head>
 <body>
+	<main>
+		<aside>
+			<div>
+				<figure>
+					<img src="" alt="Sudarsan M">
+				</figure>
 
-<div id="container">
-	<h1>Welcome to Sudharsan!</h1>
+				<div>
+					<h1>Sudarsan M</h1>
 
-	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+					<p>Software Developer</p>
+				</div>
+			</div>
+			<div>
+				<ul class="contacts-list">
+					<li>
+						<div class="icon-box">
+							<ion-icon name="mail-icon"></ion-icon>
+						</div>
 
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
+						<div>
+							<p>Email</p>
 
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
+							<a href="mailto:sudharsanm204@gmail.com">sudharsanm204@gmail.com</a>
+						</div>
+					</li>
+					<li>
+						<div>
+							<ion-icon name="number-icon"></ion-icon>
+						</div>
 
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="userguide3/">User Guide</a>.</p>
-	</div>
+						<div>
+							<p>Contact Number</p>
 
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
-</div>
+							<a href="+919345160353">+919345160353</a>
+						</div>
+					</li>
+
+					<li>
+						<div>
+							<ion-icon name="calender-outline"></ion-icon>
+						</div>
+
+						<div>
+							<p>Birthday</p>
+							
+							<time datetime="2002-10-21">October 21, 2002</time>
+						</div>
+					</li>
+
+					<li>
+						<div>
+							<ion-icon name="address"></ion-icon>
+						</div>
+
+						<div>
+							<p>Location</p>
+
+							<t>Chennai, Tamil Nadu, India</t>
+						</div>
+					</li>
+				</ul>
+
+				<div class="seperator"></div>
+
+				<ul class="social-media">
+					<li class="social-item">
+						<a href="">
+							<i class="fa-brands fa-instagram"></i>
+						</a>
+					</li>
+
+					<li class="social-item">
+						<a href="">
+							<i class="fa-brands fa-twitter"></i>
+						</a>
+					</li>
+
+					<li class="social-item">
+						<a href="">
+							<i class="fa-brands fa-threads"></i>
+						</a>
+					</li>
+
+					<li class="social-item">
+						<a href="https://github.com/Sudharsan2k20">
+							<i class="fa-brands fa-github"></i>
+						</a>
+					</li>
+				</ul>
+			</div>
+		</aside>
+	</main>
+
 
 </body>
 </html>
